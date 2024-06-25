6b2fe1d79a1e1865f9e23024b3825c2f0fda17ab
ZooSOS <wbin90620@gmail.com>
2024-06-25 06:24:43 -0700

    removed unused code useSearchParams()

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -25,7 +25,6 @@ export function ChatPanel({ messages }: ChatPanelProps) {
   const router = useRouter()
   const [isIframe, setIsIframe] = useState(false)
 
-  const searchParam = useSearchParams()
   useEffect(() => {
     if (window.location.pathname === '/iframe') {
       setIsIframe(true)
